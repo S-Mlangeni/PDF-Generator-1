7f8baf5e4a7ceacb4ee3ae7a5e94c4cbc182118c
S-Mlangeni <smlangeni21@gmail.com>
2021-10-04 12:22:48 +0200

    update

--- email.php
+++ email.php
@@ -22,9 +22,9 @@
         //Server settings - Using Gmail SMTP with TLS encryption:
         $mail->SMTPDebug = 0;//SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->Host       = 'smtp.gmail.com'; //'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->SMTPSecure = "TLS";            //Enable implicit TLS encryption
+        $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         $mail->Username   = 'tkt4332@gmail.com';//$_ENV["USERNAME"];                     //SMTP username
         $mail->Password   = 'simz1653';//$_ENV["PASSWORD"];                               //SMTP password
